41c2b1849fc90c82f4c5ede638d844597379ffb5
educep <edoo@melix.org>
2016-12-31 19:31:56 +0100

    GraphPanel añadido
    Esta clase tiene ahora el main y llama a MovParabolico y realiza el gráfico del tiro parabólico.
    Los parámetros por el momento son dados a la mano en la función ``createAndShowGui`` de la clase GraphPanel.

--- Simu/src/com/company/MovParabolico.java
+++ Simu/src/com/company/MovParabolico.java
@@ -1,25 +1,29 @@
-package com.company;
+
 
 /**
  * Created by Eduardo on 28/12/2016.
  */
-
-/**
- * Created by Eduardo on 27/12/2016.
- */
-
-
+package com.company;
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
 
 /* Program to calculate the range of a projectile,
 neglecting air resistance. */
 
-class MovParabolico {
+public class MovParabolico {
     double g = 9.8;// SI units
-    double velocidad, alturainicial, anguloEnRads;
-    double maxaltitud, maxdistance, tiempoDeVuelo;
+    public double velocidad, alturainicial, anguloEnRads;
+    public double maxaltitud, maxdistance, tiempoDeVuelo;
+    int NroPasos = 100; //para los pasos del tiempo
+    public List<Double> Tiempos;
+    public List<Double> XPos;
+    public List<Double> YPos;
+    public List<Double> XVel;
+    public List<Double> YVel;
 
-    MovParabolico(double velocidad, double anguloEnGrads, double alturainicial) {
+    public MovParabolico(double velocidad, double anguloEnGrads, double alturainicial) {
         this.anguloEnRads = anguloEnGrads * Math.PI / 180;
         this.velocidad = velocidad;
         this.alturainicial = alturainicial;
@@ -34,7 +38,12 @@ class MovParabolico {
         //Distancia maximal, el tiempo es dado por el tiempo de vuelo
         double[] Pos2 = Posicion(tiempoDeVuelo);
         this.maxdistance = Pos2[0];
+        this.Simular();
         //System.out.println("maxima distancia = " + twoDecPlaces.format(maxdistance)  + "m");
+        DecimalFormat twoDecPlaces = new DecimalFormat("0.00");
+        System.out.println("Tiempo de vuelo = " + twoDecPlaces.format(tiempoDeVuelo) + "s");
+        System.out.println("Distancia máxima = " + twoDecPlaces.format(maxdistance) + "m");
+        System.out.println("Altura máxima = " + twoDecPlaces.format(maxaltitud) + "m");
     }
 
     private double [] Posicion(double tiempo) {
@@ -56,13 +65,72 @@ class MovParabolico {
         return vel;
     }
 
+    public void Simular() {
+        List<Double> Tiempos = new ArrayList<>();
+        List<Double> XPos = new ArrayList<>();
+        List<Double> YPos = new ArrayList<>();
+        List<Double> XVel = new ArrayList<>();
+        List<Double> YVel = new ArrayList<>();
+
+        DecimalFormat twoDecPlaces = new DecimalFormat("0.00");
+        double PasoTiempo = this.tiempoDeVuelo / this.NroPasos;
+        System.out.println("Delta t = " + twoDecPlaces.format(PasoTiempo) + "s");
+
+        /*
+        double[] Tiempos = new double[NroPasos + 1];
+        double[] XPos = new double[NroPasos + 1];
+        double[] YPos = new double[NroPasos + 1];
+        double[] XVel = new double[NroPasos + 1];
+        double[] YVel = new double[NroPasos + 1];
+        */
+        double[] tPos = new double[2];
+        double[] tVel = new double[2];
+
+        for (int i = 0; i <= NroPasos; i++) {
+            Tiempos.add((double) i * PasoTiempo);
+            tPos = Posicion(Tiempos.get(i));
+            XPos.add(tPos[0]);
+            YPos.add(tPos[1]);
+            //System.out.println("Pos: t = " + twoDecPlaces.format(Tiempos.get(i)) +
+            //        "s: (" + twoDecPlaces.format(XPos.get(i)) + " ; " +
+            //        twoDecPlaces.format(YPos.get(i)) + ')');
+            tVel = Velocidad(Tiempos.get(i));
+            XVel.add(tVel[0]);
+            YVel.add(tVel[1]);
+            //System.out.println("Vel: t = " + twoDecPlaces.format(Tiempos.get(i)) +
+            //       "s: (" + twoDecPlaces.format(XVel.get(i)) + " ; " +
+            //        twoDecPlaces.format(YVel.get(i)) + ')');
+        }
+        this.Tiempos = Tiempos;
+        this.XPos = XPos;
+        this.YPos = YPos;
+        this.XVel = XVel;
+        this.YVel = YVel;
+        System.out.println("Simulación terminada, valores afectados");
+
+        /*
+        for (int i = 0; i <= NroPasos; i++) {
+            Tiempos[i] = (double) i * PasoTiempo;
+            tPos = Posicion(Tiempos[i]);
+            XPos[i] = tPos[0];
+            YPos[i] = tPos[1];
+            System.out.println("Pos: t = " + twoDecPlaces.format(Tiempos[i]) +
+                    "s: (" + twoDecPlaces.format(XPos[i]) + " ; " + twoDecPlaces.format(YPos[i]) + ')');
+            tVel = Velocidad(Tiempos[i]);
+            XVel[i] = tVel[0];
+            YVel[i] = tVel[1];
+            System.out.println("Vel: t = " + twoDecPlaces.format(Tiempos[i]) +
+                    "s: (" + twoDecPlaces.format(XVel[i]) + " ; " + twoDecPlaces.format(YVel[i]) + ')');
+        }
+        */
+    }
+
     public static void main(String[] arg) {
 
         //Parámetros de experimento
         double Velocidad = 9; //en metros / segundo
         double Angulo = 30.95; //en grados
         double Altura = 30; //en metros
-        int NroPasos = 100; //para los pasos del tiempo
 
         try {
             Velocidad = Double.parseDouble(arg[0]);
@@ -76,6 +144,7 @@ class MovParabolico {
         double TiempoMax = TiroParabolico.tiempoDeVuelo;
         double xMax = TiroParabolico.maxdistance;
         double yMax = TiroParabolico.maxaltitud;
+        int NroPasos = TiroParabolico.NroPasos;
 
         DecimalFormat twoDecPlaces = new DecimalFormat("0.00");
         System.out.println("Tiempo de vuelo = " + twoDecPlaces.format(TiempoMax) + "s");
@@ -83,33 +152,8 @@ class MovParabolico {
         System.out.println("Altura máxima = " + twoDecPlaces.format(yMax) + "m");
 
 
-        double PasoTiempo = TiempoMax / NroPasos;
-        System.out.println("Delta t = " + twoDecPlaces.format(PasoTiempo) + "s");
-
-        double [] Tiempos = new double[NroPasos + 1];
-        double [] XPos = new double[NroPasos + 1];
-        double [] YPos = new double[NroPasos + 1];
-        double [] XVel = new double[NroPasos + 1];
-        double [] YVel = new double[NroPasos + 1];
-
-        double [] tPos = new double[2];
-        double [] tVel = new double[2];
-
-        for(int i = 0; i <= NroPasos ; i++ ){
-            Tiempos[i] = PasoTiempo * i;
-            tPos = TiroParabolico.Posicion(Tiempos[i]);
-            XPos[i] = tPos[0];
-            YPos[i] = tPos[1];
-            System.out.println("Pos: t = "+ twoDecPlaces.format(Tiempos[i]) +
-                    "s: ("+ twoDecPlaces.format(XPos[i]) + " ; " + twoDecPlaces.format(YPos[i])+')'  );
-            tVel = TiroParabolico.Velocidad(Tiempos[i]);
-            XVel[i] = tVel[0];
-            YVel[i] = tVel[1];
-            System.out.println("Vel: t = "+ twoDecPlaces.format(Tiempos[i]) +
-                    "s: ("+ twoDecPlaces.format(XVel[i]) + " ; " + twoDecPlaces.format(YVel[i])+')'  );
-
 
         }
     }
-}
+
 
---
+++ Simu/src/painting/GraphPanel.java
@@ -0,0 +1,237 @@
+package painting;
+
+import com.company.MovParabolico;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Rodrigo
+ * ou can define something like
+ * int graphWidth = getWidth() - 2 * padding - labelPadding; int graphHeight = getHeight() - 2 * padding - labelPadding;
+ * to avoid calculating position every time.
+ */
+public class GraphPanel extends JPanel {
+
+    private int width = 600;
+    private int heigth = 400;
+    private int padding = 50; //25
+    private int labelPadding = 50; //25
+    private Color lineColor = new Color(16, 13, 117, 187);
+    private Color pointColor = new Color(100, 100, 100, 180);
+    private Color gridColor = new Color(216, 218, 206, 200);
+    private static final Stroke GRAPH_STROKE = new BasicStroke(2f);
+    private int pointWidth = 4;
+    private int numberYDivisions = 10;
+    private int numberXDivisions = 10;
+    private List<Double> scoresY;
+    private List<Double> scoresX;
+
+    public GraphPanel(List<Double> scoresX ,List<Double> scoresY) {
+       this.scoresY = scoresY;
+        this.scoresX = scoresX;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        double xScale = ((double) getWidth() - (2 * padding) - labelPadding) /
+                (scoresY.size() - 1);
+        double yScale = ((double) getHeight() - 2 * padding - labelPadding) /
+                (getMaxScore() - getMinScore());
+
+        List<Point> graphPoints = new ArrayList<>();
+        for (int i = 0; i < scoresY.size(); i++) {
+            int x1 = (int) (i * xScale + padding + labelPadding);
+            int y1 = (int) ((getMaxScore() - scoresY.get(i)) * yScale + padding);
+            System.out.println("x = " + scoresX.get(i) + ";" + "y = " + scoresY.get(i));
+            graphPoints.add(new Point(x1, y1));
+        }
+
+        // draw white background
+        g2.setColor(Color.WHITE);
+        g2.fillRect(padding + labelPadding, padding, getWidth() -
+                (2 * padding) - labelPadding, getHeight() - 2 * padding - labelPadding);
+        g2.setColor(Color.BLACK);
+
+        // create hatch marks and grid lines for y axis.
+        for (int i = 0; i < numberYDivisions + 1; i++) {
+            int x0 = padding + labelPadding;
+            int x1 = pointWidth + padding + labelPadding;
+            int y0 = getHeight() - ((i * (getHeight() - padding * 2 - labelPadding)) /
+                    numberYDivisions + padding + labelPadding);
+            int y1 = y0;
+            if (scoresY.size() > 0) {
+                g2.setColor(gridColor);
+                g2.drawLine(padding + labelPadding + 1 + pointWidth, y0, getWidth() - padding, y1);
+                g2.setColor(Color.BLACK);
+                String yLabel = ((int) ((getMinScore() + (getMaxScore() - getMinScore()) * ((i * 1.0) /
+                        numberYDivisions)) * 100)) / 100.0 + "";
+                FontMetrics metrics = g2.getFontMetrics();
+                int labelWidth = metrics.stringWidth(yLabel);
+                g2.drawString(yLabel, x0 - labelWidth - 5, y0 + (metrics.getHeight() / 2) - 3);
+            }
+            g2.drawLine(x0, y0, x1, y1);
+        }
+
+        // and for x axis
+        for (int i = 0; i < numberXDivisions; i++) {
+            if (scoresX.size() > 1) {
+                int x0 = i * (getWidth() - padding * 2 - labelPadding)
+                        / numberXDivisions + padding + labelPadding;
+                int x1 = x0;
+                int y0 = getHeight() - padding - labelPadding;
+                int y1 = y0 - pointWidth;
+                //if ((i % ((int) ((scoresX.size() / 20.0)) + 1)) == 0) {
+                    g2.setColor(gridColor);
+                    g2.drawLine(x0, getHeight() - padding - labelPadding -
+                            1 - pointWidth, x1, padding);
+                    g2.setColor(Color.BLACK);
+                    String xLabel = ((int) ((getXMinScore() + (getXMaxScore() - getXMinScore()) * ((i * 1.0) /
+                            numberXDivisions)) * 100)) / 100.0 + "";
+                    //String xLabel = i + "";
+                    FontMetrics metrics = g2.getFontMetrics();
+                    int labelWidth = metrics.stringWidth(xLabel);
+                    g2.drawString(xLabel, x0 - labelWidth / 2, y0 +
+                            metrics.getHeight() + 3);
+                //}
+                g2.drawLine(x0, y0, x1, y1);
+            }
+        }
+
+        /*
+        // and for x axis
+        for (int i = 0; i < scoresY.size(); i++) {
+            if (scoresY.size() > 1) {
+                int x0 = i * (getWidth() - padding * 2 - labelPadding)
+                        / (scoresY.size() - 1) + padding + labelPadding;
+                int x1 = x0;
+                int y0 = getHeight() - padding - labelPadding;
+                int y1 = y0 - pointWidth;
+                if ((i % ((int) ((scoresY.size() / 20.0)) + 1)) == 0) {
+                    g2.setColor(gridColor);
+                    g2.drawLine(x0, getHeight() - padding - labelPadding -
+                            1 - pointWidth, x1, padding);
+                    g2.setColor(Color.BLACK);
+                    String xLabel = i + "";
+                    FontMetrics metrics = g2.getFontMetrics();
+                    int labelWidth = metrics.stringWidth(xLabel);
+                    g2.drawString(xLabel, x0 - labelWidth / 2, y0 +
+                            metrics.getHeight() + 3);
+                }
+                g2.drawLine(x0, y0, x1, y1);
+            }
+        }
+    */
+
+        // create x and y axes
+        g2.drawLine(padding + labelPadding, getHeight() - padding - labelPadding,
+                padding + labelPadding, padding);
+        g2.drawLine(padding + labelPadding, getHeight() - padding - labelPadding,
+                getWidth() - padding, getHeight() - padding - labelPadding);
+
+        Stroke oldStroke = g2.getStroke();
+        g2.setColor(lineColor);
+        g2.setStroke(GRAPH_STROKE);
+
+        for (int i = 0; i < graphPoints.size() - 1; i++) {
+            int x1 = graphPoints.get(i).x;
+            int y1 = graphPoints.get(i).y;
+            int x2 = graphPoints.get(i + 1).x;
+            int y2 = graphPoints.get(i + 1).y;
+            g2.drawLine(x1, y1, x2, y2);
+        }
+
+        g2.setStroke(oldStroke);
+        g2.setColor(pointColor);
+        for (int i = 0; i < graphPoints.size(); i++) {
+            int x = graphPoints.get(i).x - pointWidth / 2;
+            int y = graphPoints.get(i).y - pointWidth / 2;
+            //System.out.println("x = " + graphPoints.get(i).x + ";" + "y = " + graphPoints.get(i).y);
+            int ovalW = pointWidth;
+            int ovalH = pointWidth;
+            g2.fillOval(x, y, ovalW, ovalH);
+        }
+    }
+
+// *   @Override
+// *   public Dimension getPreferredSize() {
+// *       return new Dimension(width, heigth);
+// *   }
+
+    private double getMinScore() {
+        double minScore = Double.MAX_VALUE;
+        for (Double score : scoresY) {
+            minScore = Math.min(minScore, score);
+        }
+        return minScore;
+    }
+
+    private double getXMinScore() {
+        double minScore = Double.MAX_VALUE;
+        for (Double score : scoresX) {
+            minScore = Math.min(minScore, score);
+        }
+        return minScore;
+    }
+
+    private double getMaxScore() {
+        double maxScore = Double.MIN_VALUE;
+        for (Double score : scoresY) {
+            maxScore = Math.max(maxScore, score);
+        }
+        return maxScore;
+    }
+
+    private double getXMaxScore() {
+        double maxScore = Double.MIN_VALUE;
+        for (Double score : scoresX) {
+            maxScore = Math.max(maxScore, score);
+        }
+        return maxScore;
+    }
+
+    public void setScores(List<Double> scores) {
+        this.scoresY = scores;
+        invalidate();
+        this.repaint();
+    }
+
+    public List<Double> getScores() {
+        return scoresY;
+    }
+
+    private static void createAndShowGui() {
+        List<Double> scores = new ArrayList<>();
+        //Random random = new Random();
+        MovParabolico TiroParabolico;
+        TiroParabolico = new MovParabolico(25, 45, 10);
+
+
+        GraphPanel mainPanel = new GraphPanel(TiroParabolico.XPos,TiroParabolico.YPos);
+        mainPanel.setPreferredSize(new Dimension(800, 600));
+        JFrame frame = new JFrame("Tiro Parabólico - CepedaHnos");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(mainPanel);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGui();
+            }
+        });
+    }
+}
